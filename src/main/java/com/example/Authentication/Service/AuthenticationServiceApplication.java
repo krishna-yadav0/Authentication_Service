229c0a97b26817df31dc176c88c62a2fdac8f539
Krishna Yadav <90216712+krishna-yadav0@users.noreply.github.com>
2024-10-06 09:10:38 +0530

    Authentication Service project initialized

---
+++ src/main/java/com/example/Authentication/Service/AuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Authentication.Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationServiceApplication.class, args);
+	}
+
+}
